f93d012dd8bcdae2a490a5256ede47e6f6496000
rahmatullakhujaev <r.xo'jayev@student.inha.uz>
2024-03-18 19:47:19 +0500

    done

--- maxway/order/urls.py
+++ maxway/order/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from . import views
 urlpatterns = [
-    path('cart', views.cart, name='cart')
-
+    path('cart', views.cart, name='cart'),
+    path('order', views.order, name='order')
 ]
--- maxway/order/views.py
+++ maxway/order/views.py
@@ -1,4 +1,18 @@
 from django.shortcuts import render
-
+import json
+from urllib.parse import unquote
+from maxway.product.models import Product
 def cart(request):
-    return render(request, 'cart.html')
+    cart = request.COOKIES.get('cart')
+    cart = unquote(cart)
+    cart = json.loads(cart)
+    products = Product.objects.filter(id__in=[item['id'] for item in cart])
+
+
+    context = {
+        'products' : products
+    }
+    return render(request, 'cart.html', context)
+
+def order(request):
+    return render(request, 'order.html')
